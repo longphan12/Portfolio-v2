24499a0c8f125efd96e5d1b6d2a1d9d551a8d90a
longphan084 <longphan084@gmail.com>
2025-06-19 22:58:18 -0500

    navbar complete

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import styles from './Navbar.module.css';
+import { AiOutlineCode, AiOutlineHome, AiOutlineUser} from "react-icons/ai";
+import { BsBriefcase } from "react-icons/bs";
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <a href='#home' className={styles.navLinks}>
+                <AiOutlineHome color='white'/>
+            </a>
+            <a href='#about' className={styles.navLinks}>
+                <AiOutlineUser color='white'/>
+            </a>
+            <a href='#experience' className={styles.navLinks}>
+                <BsBriefcase color='white'/>
+            </a>
+            <a href='#projects' className={styles.navLinks}>
+                <AiOutlineCode color='white' />
+            </a>
+        </nav>
+    )
+}
